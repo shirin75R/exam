6d985f27c9edf5a3f24ebce4865c9b9191fef756
Milad Heydari <milad.heydari@snapp.cab>
2023-03-22 20:30:16 +0330

    add setcontentview

--- app/src/main/java/com/shirin/myapp/MainActivity.kt
+++ app/src/main/java/com/shirin/myapp/MainActivity.kt
@@ -15,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
         recyclerRick = findViewById(R.id.recyclerView)
 
 
