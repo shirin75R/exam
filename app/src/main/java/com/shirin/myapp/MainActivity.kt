2efa06fcc4f82ab6e784ccadb4c81b2acc2e26e6
Milad Heydari <miladheydari2011@gmail.com>
2023-03-22 18:11:56 +0330

    ddd

--- app/src/main/java/com/shirin/myapp/MainActivity.kt
+++ app/src/main/java/com/shirin/myapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.shirin.myapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var layoutManager: RecyclerView.LayoutManager? =null
+    private var adapter :RecyclerView.Adapter<RecyclerAdapter.ViewHolder>?=null
+
+
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+
+        layoutManager=LinearLayoutManager(this)
+        findViewById<RecyclerView>(R.id.recyclerView).layoutManager=layoutManager
+        adapter=RecyclerAdapter()
+        findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
+
     }
 }
--- app/src/main/java/com/shirin/myapp/RecyclerAdapter.kt
+++ app/src/main/java/com/shirin/myapp/RecyclerAdapter.kt
@@ -1,33 +1,51 @@
 package com.shirin.myapp
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerAdapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
-        TODO("Not yet implemented")
+
+    private var title = arrayOf("Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8")
+    private var details = arrayOf("item 1 details","item 2 1details","item 3 details","item 4 details","item 5 details","item 6 details","item 7 details","item 8 details")
+    //private var images = intArrayOf(R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,)
+    private var images ="https://rickandmortyapi.com/api/character"
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v =LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return title.size
+
     }
 
-    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemTitle.text=title[position]
+        holder.itemDetail.text=details[position]
+      //  holder.itemImage.setImageResource(images[position])
+
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var itemImage : ImageView
+        val itemImage = "https://rickandmortyapi.com/api/character"
+     //   var itemImage : ImageView
         var itemTitle : TextView
         var itemDetail : TextView
         init {
-            itemImage=itemView.findViewById(R.id.item_image)
+         //   itemImage=itemView.findViewById(R.id.item_image)
             itemTitle=itemView.findViewById(R.id.item_title)
             itemDetail=itemView.findViewById(R.id.item_detail)
+
+          /*  if (itemImage !== null) {
+                Glide.with(this)
+                    .load(itemImage)
+                    .into(itemView.findViewById(R.id.item_image))
+            }
+*/
+
 
         }
 
