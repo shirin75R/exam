558381ff882268d2ced9b289ea7c5e893d63b670
Milad Heydari <milad.heydari@snapp.cab>
2023-03-22 20:25:49 +0330

    refactor

--- app/src/main/java/com/shirin/myapp/MainActivity.kt
+++ app/src/main/java/com/shirin/myapp/MainActivity.kt
@@ -8,20 +8,20 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private var layoutManager: RecyclerView.LayoutManager? =null
-    private var adapter :RecyclerView.Adapter<RecyclerAdapter.ViewHolder>?=null
-
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    lateinit var recyclerRick: RecyclerView
 
     @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        recyclerRick = findViewById(R.id.recyclerView)
 
 
-
-        layoutManager=LinearLayoutManager(this)
-        findViewById<RecyclerView>(R.id.recyclerView).layoutManager=layoutManager
-        adapter=RecyclerAdapter()
-        findViewById<RecyclerView>(R.id.recyclerView).adapter = adapter
+        layoutManager = LinearLayoutManager(this)
+        recyclerRick.layoutManager = layoutManager // better only define once recycler view in class similar adapter
+        adapter = RecyclerAdapter()
+        recyclerRick.adapter = adapter
 
     }
 }
--- app/src/main/java/com/shirin/myapp/RecyclerAdapter.kt
+++ app/src/main/java/com/shirin/myapp/RecyclerAdapter.kt
@@ -6,14 +6,33 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private var title = arrayOf("Chapter1","Chapter2","Chapter3","Chapter4","Chapter5","Chapter6","Chapter7","Chapter8")
-    private var details = arrayOf("item 1 details","item 2 1details","item 3 details","item 4 details","item 5 details","item 6 details","item 7 details","item 8 details")
+    private var title = arrayOf(
+        "Chapter1",
+        "Chapter2",
+        "Chapter3",
+        "Chapter4",
+        "Chapter5",
+        "Chapter6",
+        "Chapter7",
+        "Chapter8"
+    )
+    private var details = arrayOf(
+        "item 1 details",
+        "item 2 1details",
+        "item 3 details",
+        "item 4 details",
+        "item 5 details",
+        "item 6 details",
+        "item 7 details",
+        "item 8 details"
+    )
+
     //private var images = intArrayOf(R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,R.drawable.download,)
-    private var images ="https://rickandmortyapi.com/api/character"
+    private var images = "https://rickandmortyapi.com/api/character"
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v =LayoutInflater.from(parent.context).inflate(R.layout.card_layout,parent,false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
         return ViewHolder(v)
     }
 
@@ -23,28 +42,30 @@ class RecyclerAdapter:RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemTitle.text=title[position]
-        holder.itemDetail.text=details[position]
-      //  holder.itemImage.setImageResource(images[position])
+        holder.itemTitle.text = title[position]
+        holder.itemDetail.text = details[position]
+        //  holder.itemImage.setImageResource(images[position])
 
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val itemImage = "https://rickandmortyapi.com/api/character"
-     //   var itemImage : ImageView
-        var itemTitle : TextView
-        var itemDetail : TextView
-        init {
-         //   itemImage=itemView.findViewById(R.id.item_image)
-            itemTitle=itemView.findViewById(R.id.item_title)
-            itemDetail=itemView.findViewById(R.id.item_detail)
 
-          /*  if (itemImage !== null) {
-                Glide.with(this)
-                    .load(itemImage)
-                    .into(itemView.findViewById(R.id.item_image))
-            }
-*/
+        //   var itemImage : ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            //   itemImage=itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
+
+            /*  if (itemImage !== null) {
+                  Glide.with(this)
+                      .load(itemImage)
+                      .into(itemView.findViewById(R.id.item_image))
+              }
+  */
 
 
         }
